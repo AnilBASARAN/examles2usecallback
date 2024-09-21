77b5639f04c531f1a227f29d30e2200360da989a
Anıl Başaran <nlbsrn@gmail.com>
2024-09-22 00:40:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Example1 from "./pages/Example1";
+import Example2 from "./pages/Example2";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="flex justify-around py-8 text-lg shadow-lg text-rose-300 bg-rose-700">
+        <Link to="/">Example 1</Link>
+        <Link to="/example-2">Example 2</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Example1 />} />
+        <Route path="/example-2" element={<Example2 />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
